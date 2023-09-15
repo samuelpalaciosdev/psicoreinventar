76f45811a1410ee9a0017e263ee3a1782b0f12ce
Samuel Palacios <samuelpl0888@gmail.com>
2023-09-15 16:28:32 -0400

    update: specialists page components redesign

--- components/button-select-doctor.tsx
+++ components/button-select-doctor.tsx
@@ -3,16 +3,21 @@
 import { Button } from './ui/button';
 import { useAppointmentStore } from '@/context/store';
 
-type ButtonSelectDoctorProps = {
+interface ButtonSelectDoctorProps extends React.HTMLAttributes<HTMLButtonElement> {
   doctorId: string;
-};
+}
 
-export default function ButtonSelectDoctor({ doctorId }: ButtonSelectDoctorProps) {
+export default function ButtonSelectDoctor({
+  className,
+  doctorId,
+  ...props
+}: ButtonSelectDoctorProps) {
   const setDoctor = useAppointmentStore((state) => state.setDoctor);
 
   return (
     <Button
       type='button'
+      className={className}
       onClick={() => {
         setDoctor(doctorId);
       }}
--- components/doctor-card.tsx
+++ components/doctor-card.tsx
@@ -20,18 +20,19 @@ export default function DoctorCard({
   doctorEducation,
 }: DoctorCardProps) {
   return (
-    <Card className='w-[18rem] min-h-fit py-4'>
-      <CardHeader className='flex flex-row gap-2 items-center justify-center'>
+    <Card className='w-[22rem] min-h-fit py-2'>
+      <CardHeader className='flex flex-row gap-4 items-center'>
         <Image src={image} alt={name} className='rounded-full' width={72} height={72} />
         <div className='flex flex-col gap-2'>
-          <CardTitle>Psychologist {name}</CardTitle>
+          <p className='text-sm font-medium text-muted-foreground'>Psychologist</p>
+          <CardTitle>{name}</CardTitle>
           <CardDescription>Specialty: {doctorSpecialty}</CardDescription>
         </div>
       </CardHeader>
-      <CardContent>
+      <CardContent className='flex flex-col gap-2'>
         <p className='text-muted-foreground text-sm'>{doctorExperience}</p>
         <p className='text-muted-foreground text-sm'>{doctorEducation}</p>
-        <ButtonSelectDoctor doctorId={id} />
+        <ButtonSelectDoctor doctorId={id} className='self-start' />
       </CardContent>
     </Card>
   );
