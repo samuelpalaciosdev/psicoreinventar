a23eeda001f50cbec3f7702f7803c4fe16c5a162
Samuel Palacios <samuelpl0888@gmail.com>
2023-09-18 04:24:32 -0400

    add: datetime picker onChange handler

--- components/appointment/datetime-picker.tsx
+++ components/appointment/datetime-picker.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useState } from 'react';
 import dayjs, { Dayjs } from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -18,6 +19,17 @@ export default function DateTimePickerAppointment() {
     return day === 0 || day === 6;
   };
 
+  // Format date like Thursday, September 21, 2023 8:00 AM
+  const formatDateTime = (date: Dayjs) => {
+    return date.format('dddd, MMMM D, YYYY h:mm A');
+  };
+
+  const [value, setValue] = useState<Dayjs | null>(eightAm);
+  const [formattedValue, setFormattedValue] = useState<string>(
+    formatDateTime(eightAm) // Initialize with formatted initial value
+  );
+  // console.log(formattedValue);
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DateTimePicker
@@ -25,13 +37,17 @@ export default function DateTimePickerAppointment() {
         viewRenderers={{
           hours: renderTimeViewClock,
         }}
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+          setFormattedValue(formatDateTime(newValue!));
+        }}
         minDate={today}
         maxDate={oneWeek}
         shouldDisableDate={isWeekend}
         minTime={eightAm}
         maxTime={fivePm}
         minutesStep={60}
-        // Need this style so that the popup is clickable inside the dialog component
         slotProps={{
           popper: {
             sx: {
