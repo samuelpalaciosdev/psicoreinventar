d3a2b01742b5eba7299dcbf1ae03cf62bdbc4694
Samuel Palacios <samuelpl0888@gmail.com>
2023-09-18 03:13:28 -0400

    fix: datetime picker working

--- components/appointment/datetime-picker.tsx
+++ components/appointment/datetime-picker.tsx
@@ -1,13 +1,12 @@
 'use client';
-import { useState } from 'react';
 import dayjs, { Dayjs } from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { renderTimeViewClock } from '@mui/x-date-pickers/timeViewRenderers';
 
 export default function DateTimePickerAppointment() {
+  const today = dayjs();
   const oneWeek = dayjs().add(7, 'day');
 
   const eightAm = dayjs().set('hour', 8).startOf('hour');
@@ -26,13 +25,13 @@ export default function DateTimePickerAppointment() {
         viewRenderers={{
           hours: renderTimeViewClock,
         }}
-        defaultValue={eightAm}
+        minDate={today}
+        maxDate={oneWeek}
         shouldDisableDate={isWeekend}
-        disablePast
         minTime={eightAm}
         maxTime={fivePm}
-        maxDate={oneWeek}
         minutesStep={60}
+        // Need this style so that the popup is clickable inside the dialog component
         slotProps={{
           popper: {
             sx: {
