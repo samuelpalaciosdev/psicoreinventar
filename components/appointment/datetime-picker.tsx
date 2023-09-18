a6516db0839b2a5f8a1aca509e71dddcfe9b3afb
Samuel Palacios <samuelpl0888@gmail.com>
2023-09-18 02:50:26 -0400

    test: datetimepicker component being tricky

--- components/appointment/datetime-picker.tsx
+++ components/appointment/datetime-picker.tsx
@@ -1,16 +1,17 @@
 'use client';
+import { useState } from 'react';
 import dayjs, { Dayjs } from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { renderTimeViewClock } from '@mui/x-date-pickers/timeViewRenderers';
 
 export default function DateTimePickerAppointment() {
   const oneWeek = dayjs().add(7, 'day');
 
-  const eightAM = dayjs().set('hour', 8).startOf('hour');
-  const fivePM = dayjs().set('hour', 17).startOf('hour');
+  const eightAm = dayjs().set('hour', 8).startOf('hour');
+  const fivePm = dayjs().set('hour', 17).startOf('hour');
 
   const isWeekend = (date: Dayjs) => {
     const day = date.day();
@@ -20,30 +21,27 @@ export default function DateTimePickerAppointment() {
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={['DateTimePicker']}>
-        <DateTimePicker
-          viewRenderers={{
-            hours: renderTimeViewClock,
-          }}
-          className='self-start z-[100] pointer-events-auto'
-          autoFocus={true}
-          disablePast={true}
-          minutesStep={60}
-          maxDate={oneWeek}
-          shouldDisableDate={isWeekend}
-          defaultValue={eightAM}
-          minTime={eightAM}
-          // Need this styling to make the popper work inside the dialog component
-          slotProps={{
-            popper: {
-              sx: {
-                '&.MuiPopper-root': { pointerEvents: 'auto' },
-              },
-              placement: 'top',
+      <DateTimePicker
+        className='self-start'
+        viewRenderers={{
+          hours: renderTimeViewClock,
+        }}
+        defaultValue={eightAm}
+        shouldDisableDate={isWeekend}
+        disablePast
+        minTime={eightAm}
+        maxTime={fivePm}
+        maxDate={oneWeek}
+        minutesStep={60}
+        slotProps={{
+          popper: {
+            sx: {
+              '&.MuiPopper-root': { pointerEvents: 'auto' },
             },
-          }}
-        />
-      </DemoContainer>
+            placement: 'top',
+          },
+        }}
+      />
     </LocalizationProvider>
   );
 }
